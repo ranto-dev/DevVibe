6a744450ac7179d56d524479070c88d4dd6158f9
ranto-dev <rantoandrianandraina@gmail.com>
2026-03-09 08:20:06 +0000

    feat: add main navigation and base activities for DevVibe

--- DevVibe/app/src/main/java/com/ranto/devvibe/MainActivity.kt
+++ DevVibe/app/src/main/java/com/ranto/devvibe/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.ranto.devvibe
-
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnPomodoro = findViewById<Button>(R.id.btnPomodoro)
+        val btnMusic = findViewById<Button>(R.id.btnMusic)
+        val btnStats = findViewById<Button>(R.id.btnStats)
+
+        btnPomodoro.setOnClickListener {
+            startActivity(Intent(this, PomodoroActivity::class.java))
+        }
+
+        btnMusic.setOnClickListener {
+            startActivity(Intent(this, MusicActivity::class.java))
+        }
+
+        btnStats.setOnClickListener {
+            startActivity(Intent(this, StatsActivity::class.java))
         }
     }
 }
---
+++ DevVibe/app/src/main/java/com/ranto/devvibe/StatsActivity.kt
@@ -0,0 +1,15 @@
+package com.ranto.devvibe
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class StatsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_stats)
+    }
+}
