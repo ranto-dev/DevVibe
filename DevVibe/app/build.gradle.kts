924f06358d8182d9734c0ab7bd6b571346e108bf
ranto-dev <rantoandrianandraina@gmail.com>
2026-03-13 08:19:13 +0000

    feat:materialiew the UI

--- DevVibe/app/build.gradle.kts
+++ DevVibe/app/build.gradle.kts
@@ -51,4 +51,5 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.4.0")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.google.android.material:material:1.11.0")
 }
